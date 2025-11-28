d51fb5c348a3029b7517b8001e05b0d7bce00c48
Kevin Sanches <kevin@Kevins-Air-3.lan>
2025-11-27 23:21:24 -0500

    implement remove student from campus

--- src/components/views/CampusView.js
+++ src/components/views/CampusView.js
@@ -8,7 +8,7 @@ import { Link } from "react-router-dom";
 
 // Take in props data to construct the component
 const CampusView = (props) => {
-  const {campus} = props;
+  const {campus, removeStudentFromCampus} = props;
   
   // Render a single Campus view with list of its students
   return (
@@ -22,7 +22,10 @@ const CampusView = (props) => {
           <div key={student.id}>
             <Link to={`/student/${student.id}`}>
               <h2>{name}</h2>
-            </Link>             
+            </Link>   
+            <button onClick={() => removeStudentFromCampus(student.id)}>
+                Remove from Campus
+              </button>            
           </div>
         );
       })}
