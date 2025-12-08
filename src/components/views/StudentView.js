cfb8b1d3a112c97b78e29c201d311d51aeca2906
Kevin Sanches <kevin@Kevins-Air-3.lan>
2025-12-07 22:15:13 -0500

    student's info can now be seen in single student view

--- src/components/views/StudentView.js
+++ src/components/views/StudentView.js
@@ -5,6 +5,7 @@ The Views component is responsible for rendering web page with data provided by 
 It constructs a React component to display the single student view page.
 ================================================== */
 import { Link } from "react-router-dom";
+
 const StudentView = (props) => {
   const { student } = props;
 
@@ -12,15 +13,35 @@ const StudentView = (props) => {
   return (
     <div>
       <h1>{student.firstname + " " + student.lastname}</h1>
-      <h3>{student.campus ? student.campus.name : "No Campus Assigned"}</h3>
+      
+      {student.imageUrl && (
+        <img 
+          src={student.imageUrl} 
+          alt={`${student.firstname} ${student.lastname}`} 
+          style={{ maxWidth: "300px" }}
+        />
+      )}
+      
+      <p><strong>Email:</strong> {student.email}</p>
+      
+      <p><strong>GPA:</strong> {student.gpa || "N/A"}</p>
+      
+      <h3>
+        Campus: 
+        {student.campus ? (
+          <Link to={`/campus/${student.campus.id}`}>
+            {student.campus.name}
+          </Link>
+        ) : (
+          "No Campus Assigned"
+        )}
+      </h3>
+      
       <Link to={`/student/${student.id}/edit`}>
         <button>Edit Student</button>
       </Link>
-  
     </div>
-
   );
-
 };
 
 export default StudentView;
